6896e2a298357704f6e06d2393f8ffe003807dc6
GaoXteen <thai.nguyenhong90@gmail.com>
2020-12-08 09:03:24 +0700

    edit filter

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -9,6 +9,8 @@ exports.paginate = async (req, res, next) => {
     console.log("brand="+brand);
     let type = req.query.laptop_type;
     console.log("type="+type);
+    console.log("page="+req.query.page);
+    console.log("name="+req.query.searchName);
     const returnObject = await laptopModel.getPerPage(req.query.page);
     console.log(returnObject);
     res.render('books/catalog', {laptops: returnObject.laptops,
@@ -21,3 +23,30 @@ exports.paginate = async (req, res, next) => {
         last: returnObject.last,
         pages: returnObject.pages});
 }
+
+/*exports.searchbyName = async (req, res, next) => {
+    var brand = req.query.laptop_brand;
+    var type = req.query.laptop_type;
+    if (brand == "All brands")
+    {
+        brand = '';
+    }
+    console.log("page="+req.query.page);
+    console.log("name="+req.query.searchName);
+    console.log("brand="+brand);
+    console.log("type="+type);
+
+    const returnObject = await laptopModel.searchName(req.query.page, req.query.searchName, type, brand);
+    res.render('laptops/product', {laptops: returnObject.laptops,
+        first: returnObject.first,
+        prev: returnObject.prev,
+        prevPage: returnObject.prevPage,
+        page: returnObject.Page,
+        next: returnObject.next,
+        nextPage: returnObject.nextPage,
+        last: returnObject.last,
+        pages: returnObject.pages,
+        searchName: req.query.searchName,
+        laptop_type: type,
+        laptop_brand: brand});
+}*/
--- models/laptopModel.js
+++ models/laptopModel.js
@@ -40,3 +40,60 @@ exports.getPerPage = async (page) => {
     return ret;
 };
 
+/*Search By Name*/
+/*exports.searchName = async(page, nameV, typeV, brandV) => {
+    const laptopCollection = db().collection('laptops');
+    // const laptop = await laptopCollection.findOne({name: name});
+    // return laptop;
+    // const laptops = await laptopCollection.find({name: {$regex : ".*" + nameV + ".*"}}).toArray();
+    let perPage = 5;
+    let Page = +page || 1;
+    let pages;
+    let laptops
+    // if(nameV){
+    //     pages = Math.ceil(await laptopCollection.find({name: {$regex : ".*" + nameV + ".*"}}).count() / perPage);
+    //     laptops = await laptopCollection.find({name: {$regex : ".*" + nameV + ".*"}}) // find tất cả các data
+    //     .skip((perPage * Page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+    //     .limit(perPage).toArray();
+    // }
+    // else{
+    //     pages = Math.ceil(await laptopCollection.find({}).count() / perPage);
+
+    //     laptops = await laptopCollection.find({}) // find tất cả các data
+    //     .skip((perPage * Page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+    //     .limit(perPage).toArray();
+    // }
+
+    if(nameV){
+        pages = Math.ceil(await laptopCollection.find({name: {$regex : ".*" + nameV + ".*"}, type: {$regex : ".*" + typeV + ".*"}, brand: {$regex : ".*" + brandV + ".*"}}).count() / perPage);
+        laptops = await laptopCollection.find({name: {$regex : ".*" + nameV + ".*"}, type: {$regex : ".*" + typeV + ".*"}, brand: {$regex : ".*" + brandV + ".*"}}) // find tất cả các data
+        .skip((perPage * Page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+        .limit(perPage).toArray();
+    }
+    else if (!nameV && (brandV || typeV))
+    {
+        pages = Math.ceil(await laptopCollection.find({type: {$regex : ".*" + typeV + ".*"}, brand: {$regex : ".*" + brandV + ".*"}}).count() / perPage);
+        laptops = await laptopCollection.find({type: {$regex : ".*" + typeV + ".*"}, brand: {$regex : ".*" + brandV + ".*"}}) // find tất cả các data
+        .skip((perPage * Page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+        .limit(perPage).toArray();
+    }
+    else{
+        pages = Math.ceil(await laptopCollection.find({}).count() / perPage);
+
+        laptops = await laptopCollection.find({}) // find tất cả các data
+        .skip((perPage * Page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+        .limit(perPage).toArray();
+    }
+    
+    
+    const prev = Page > 1;
+    const first = Page > 2;
+    const prevPage = Page - 1;
+    const nextPage = Page + 1;
+    const next = Page < pages;
+    const last = Page < (pages - 1);
+
+    const ret = {laptops: laptops, first:first, prev: prev, prevPage:prevPage, Page: Page, nextPage: nextPage, next: next, last: last, pages:pages}
+    return ret;
+}*/
+
