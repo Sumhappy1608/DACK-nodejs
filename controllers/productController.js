5b40aff2a18e6dc8d9f40bec0aad76674975ca38
Bach <doanxuanbach132@gmai.com>
2020-12-27 21:54:54 +0700

    design comment

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const laptopModel = require('../models/laptopModel');
 exports.index = async (req, res, next) => {
     let id = req.query.product;
     let laptop = await laptopModel.get(id);
-    console.log(laptop);
+    //console.log(laptop);
     res.render('books/product', {laptop});
 }
 
--- routers/product.js
+++ routers/product.js
@@ -9,4 +9,9 @@ router.get('/back', (req, res) => {
     res.redirect('/');
 });
 
+router.post('/comment', (req, res) => {
+    console.log(req.body.comment);
+    res.redirect('/product?product=' + req.query.product);
+});
+
 module.exports = router;
