3729abc263051210a871d4e25ac3459e74fd2776
Bach <doanxuanbach132@gmai.com>
2021-01-05 18:46:10 +0700

    .

--- models/userModel.js
+++ models/userModel.js
@@ -18,8 +18,7 @@ const transporter = nodemailer.createTransport({
 exports.registration = async(user) => {
     const user_data = {
         user: user,
-        isLock: 0,
-        buy_id: ""
+        isLock: false,
     }
     const userDatabase = db().collection('user');
     await userDatabase.insert(user_data);
